7500caf09a0e760dc49d30eb700e1343ab2801b8
Yoshi Tacuchi <yossitacuchi@gmail.com>
2023-12-11 15:16:04 -0500

    mapa

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
 jQuery(document).ready(function ($) {
+    detectNav();
     $(window).on("scroll", function () {
+        detectNav();
+    });
+
+    $('.btn-play').on('click', function() {
+        $('video').trigger('play');
+        $('.video-poster').removeClass('bg-video');
+  
+        $('.btn-play').toggleClass('hidden');
+        $('.btn-pause').toggleClass('hidden');
+      });
+  
+      $('.btn-pause').on('click', function() {
+        $('video').trigger('pause');
+        $('.video-poster').addClass('bg-video');
+  
+        $('.btn-play').toggleClass('hidden');
+        $('.btn-pause').toggleClass('hidden');
+      });
+    
+    function detectNav(){
         if ($(document).scrollTop() > 50) {
             $(".navbar").addClass('bg-light').removeClass('bg-transparent');
             $('.btn-contacto1').removeClass('hide');
@@ -10,5 +31,5 @@ jQuery(document).ready(function ($) {
             $('.btn-contacto1').addClass('hide');
             $('.btn-contacto2').removeClass('hide');
         }
-    })
+    }
 });
