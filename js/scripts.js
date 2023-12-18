32c965a217265bb2ce8d89eb5d682bad5569ae9f
Yoshi Tacuchi <yossitacuchi@gmail.com>
2023-12-18 00:22:36 -0500

    contacto

--- js/scripts.js
+++ js/scripts.js
@@ -21,15 +21,21 @@ jQuery(document).ready(function ($) {
       });
     
     function detectNav(){
-        if ($(document).scrollTop() > 50) {
-            $(".navbar").addClass('bg-light').removeClass('bg-transparent');
+        if(!$('.navbar').hasClass('navbar-info')){
+            if ($(document).scrollTop() > 50) {
+                $(".navbar").addClass('bg-light').removeClass('bg-transparent');
+                $('.btn-contacto1').removeClass('hide');
+                $('.btn-contacto2').addClass('hide');
+            }
+            else {
+                $(".navbar").addClass('bg-transparent').removeClass('bg-light');
+                $('.btn-contacto1').addClass('hide');
+                $('.btn-contacto2').removeClass('hide');
+            }
+        }else{
             $('.btn-contacto1').removeClass('hide');
             $('.btn-contacto2').addClass('hide');
         }
-        else {
-            $(".navbar").addClass('bg-transparent').removeClass('bg-light');
-            $('.btn-contacto1').addClass('hide');
-            $('.btn-contacto2').removeClass('hide');
-        }
+        
     }
 });
