586fc4566367a1ffc29568af6e0ff340031129ad
YoshiTacuchi <yossitacuchi@gmail.com>
2024-01-18 16:54:15 -0500

    map

--- js/mymap.js
+++ js/mymap.js
@@ -25,9 +25,9 @@ var globe = planetaryjs.planet();
   globe.loadPlugin(planetaryjs.plugins.pings());
   // The `zoom` and `drag` plugins enable
   // manipulating the globe with the mouse.
-  globe.loadPlugin(planetaryjs.plugins.zoom({
-    scaleExtent: [100, 300]
-  }));
+  // globe.loadPlugin(planetaryjs.plugins.zoom({
+  //   scaleExtent: [100, 300]
+  // }));
   globe.loadPlugin(planetaryjs.plugins.drag({
     // Dragging the globe should pause the
     // automatic rotation until we release the mouse.
